f1b73f4004ceca44e534145d685b3a24440ff896
silasbispo01 <silasbispo01@gmail.com>
2023-10-17 16:54:54 -0300

    chore: add authenticate use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,38 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+interface AutheticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AutheticateUseCaseResponse {
+  user: User
+}
+
+export class AutheticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AutheticateUseCaseRequest): Promise<AutheticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
