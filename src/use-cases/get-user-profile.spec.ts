8d2698a0f5d301dcc5792b5dfbc90b6c55fecf8b
silasbispo01 <silasbispo01@gmail.com>
2023-10-18 14:24:00 -0300

    test: add get user profile use case unit test

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,45 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { hash } from 'bcryptjs'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      email: 'johndoe@example.com',
+      name: 'John',
+      password_hash: await hash('password', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('John')
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    await usersRepository.create({
+      email: 'johndoe@example.com',
+      name: 'John',
+      password_hash: await hash('password', 6),
+    })
+
+    await expect(() =>
+      sut.execute({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
