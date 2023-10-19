8a43c75ab9f547c238892e8253564afdff23d5a5
silasbispo01 <silasbispo01@gmail.com>
2023-10-19 14:10:31 -0300

    test: create create-gym use case unit test

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,29 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { RegisterUseCase } from '@/use-cases/register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: null,
+      latitude: 0,
+      longitude: 0,
+      phone: null,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
