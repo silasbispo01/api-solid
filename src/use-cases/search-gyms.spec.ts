34de05c19d264dde3f83870407154adb56ec955d
silasbispo01 <silasbispo01@gmail.com>
2023-11-13 18:34:20 -0300

    test: create seach gyms use case unit test

---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,64 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { SearchGymsUseCase } from './search-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: SearchGymsUseCase
+
+describe('Seach Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new SearchGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch check-in history', async () => {
+    await gymsRepository.create({
+      title: 'JavaScript Gym',
+      description: '',
+      latitude: new Decimal(-13.0025568),
+      longitude: new Decimal(-38.4585781),
+      phone: '',
+    })
+
+    await gymsRepository.create({
+      title: 'Typescript Gym',
+      description: '',
+      latitude: new Decimal(-13.0025568),
+      longitude: new Decimal(-38.4585781),
+      phone: '',
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 1,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
+  })
+
+  it('should be able to fetch paginated gym search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `JavaScript Gym ${i}`,
+        description: '',
+        latitude: new Decimal(-13.0025568),
+        longitude: new Decimal(-38.4585781),
+        phone: '',
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JavaScript Gym 21' }),
+      expect.objectContaining({ title: 'JavaScript Gym 22' }),
+    ])
+  })
+})
