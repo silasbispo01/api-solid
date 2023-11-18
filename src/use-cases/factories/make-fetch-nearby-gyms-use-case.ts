62d99690e2fed23eb922741954da83bc5e18245c
silasbispo01 <silasbispo01@gmail.com>
2023-11-18 10:47:30 -0300

    chore: add fetch nearby gyms controller

--- src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
+++ src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
 import { FetchNearbyGymsUseCase } from '../fetch-nearby-gyms'
 
-export function makeFetchNearbyGymsUse() {
+export function makeFetchNearbyGymsUseCase() {
   const gymsRepository = new PrismaGymsRepository()
   const useCase = new FetchNearbyGymsUseCase(gymsRepository)
 
