d2a43565cf9226c92ac48c14b8e2e7a30e3806e9
silasbispo01 <silasbispo01@gmail.com>
2023-11-19 11:09:59 -0300

    feat: add check-ins routes

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
-import { gymsRoutes } from './http/controllers/gyms/routes'
-import { usersRoutes } from './http/controllers/users/routes'
+
+import { checkInsRoutes } from '@/http/controllers/check-ins/routes'
+import { gymsRoutes } from '@/http/controllers/gyms/routes'
+import { usersRoutes } from '@/http/controllers/users/routes'
 
 export const app = fastify()
 
@@ -13,6 +15,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
