d4b84c7febf3a6dc1b82ecbab0ad947977975cc3
silasbispo01 <silasbispo01@gmail.com>
2023-11-19 11:07:22 -0300

    chore: add history check-ins controller

--- src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
+++ src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 import { FetchUserCheckinsHistoryUseCase } from '../fetch-user-check-ins-history'
 
-export function makeFetchUserCheckinsHistoryUseCase() {
+export function makeFetchUserCheckInsHistoryUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
   const useCase = new FetchUserCheckinsHistoryUseCase(checkInsRepository)
 
